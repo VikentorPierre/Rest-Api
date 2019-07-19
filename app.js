33f44b0a6e9333c20ee68abb5d4555b99d8afc7a
Mosdevv <vikentorpierre@gmail.com>
2019-07-19 18:06:19 -0400

    int commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+
+// Init EndPoint
+const indexRouter = require("./Api/routes/index");
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes which should handle requests
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {}
+  });
+});
+
+module.exports = app;
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
---
+++ todo.md
@@ -0,0 +1,18 @@
+## Title
+
+- [] Thing to do
+
+---
+
+echo "# Rest-Api" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/VikentorPierre/Rest-Api.git
+git push -u origin master
+
+## Setup Rest Api
+
+- [] run "if you have express genarator install run: express --no-view than go to directore than run npm install "
+
+* [] make the folder structure and go to the app.js file and clean bit make the todo.md file and gitignore file
